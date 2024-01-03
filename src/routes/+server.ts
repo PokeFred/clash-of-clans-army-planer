ff6bc15db8b0cfe6ef89d1560559812780cdc394
PokeFred <79151797+PokeFred@users.noreply.github.com>
2024-01-03 18:48:29 +0100

    try to add static db to vercel

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,5 +1,14 @@
 import type { RequestHandler } from './$types'
+import Database from 'better-sqlite3'
 
 export const POST: RequestHandler = () => {
-    return new Response('Hello World')
+    const db: Database.Database = new Database(":memory:")
+    db.prepare("CREATE TABLE test (id INTEGER)").run()
+
+    const smt: Database.Statement = db.prepare("INSERT INTO test (id) VALUES ($id)")
+    smt.run({ id: 0 })
+    smt.run({ id: 1 })
+    smt.run({ id: 2 })
+
+    return new Response(JSON.stringify(db.prepare("SELECT * FROM test").all()))
 }
